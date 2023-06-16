65ec48efc9a098df5a87627cc43fc9f2420bb543
Olakunle64 <olakunleisiaq50@gmail.com>
2023-06-16 08:13:18 +0000

    case sensitive is very important

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", sizeof(j));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(i));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(k));
-	printf("size of a float: %lu byte(s)\n", sizeof(fl));
+	printf("Size of a float: %lu byte(s)\n", sizeof(fl));
 	return (0);
 }
