946a1072f5db8ae54e433468a9357e824b9bd633
Olakunle64 <olakunleisiaq50@gmail.com>
2023-06-16 06:17:18 +0000

    use printf and puts to print to standard output. also use the size of operator to print the number of each data type

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print a string
+ * Description: This is meant to use puts to print a
+ * string to standard output
+ * Return: return zero
+ */
+int main(void)
+{
+	char str[] = "Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print a string
+ * Description: use the printf to print a string to
+ * the standard output
+ * Return: return zero
+ */
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", str);
+	return (0);
+}
