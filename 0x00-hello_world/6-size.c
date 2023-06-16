7d2e0024eb5ec4fd3d83d65e44d07749446d139d
Olakunle64 <olakunleisiaq50@gmail.com>
2023-06-16 07:51:56 +0000

    declare a data types before checking the size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,16 @@
  */
 int main(void)
 {
-	printf("size of a char: %zu byte(s)\n", sizeof(char));
-	printf("size of an int: %zu byte(s)\n", sizeof(int));
-	printf("size of a long int: %ld byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("size of a float: %zu byte(s)\n", sizeof(float));
+	char c;
+	int num;
+	long int ln;
+	long long int lln;
+	float fl;
+
+	printf("size of a char: %zu byte(s)\n", sizeof(c));
+	printf("size of an int: %zu byte(s)\n", sizeof(num));
+	printf("size of a long int: %zu byte(s)\n", sizeof(ln));
+	printf("size of a long long int: %zu byte(s)\n", sizeof(lln));
+	printf("size of a float: %zu byte(s)\n", sizeof(fl));
 	return (0);
 }
