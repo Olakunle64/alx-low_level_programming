1d5717463a3d919b5dd4a8dc6673f43b71124e8b
Olakunle64 <olakunleisiaq50@gmail.com>
2023-06-21 07:15:15 +0000

    comment the argument of the function in question 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * print_last_digit - print the last digit of a number
- * @int: argument
+ * @a: argument
  *
  * Description: The function is meant to print the last
  * digit of a given number
  * Return: return 1 on success
  */
-int print_last_digit(int)
+int print_last_digit(int a)
 {
 	return (a % 10);
 }
