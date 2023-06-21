7891dd2e87a09d29eeb798d957521557901eb609
Olakunle64 <olakunleisiaq50@gmail.com>
2023-06-21 07:07:34 +0000

    correct the improper logical operator in the if statement of question 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,13 +3,13 @@
  * _isalpha - check for alphabetic character
  * @c: argument
  *
- * Description: This function is meant to chek for alphabetic character
- * and return the appropriate value
+ * Description: This function is meant to check for
+ * alphabetic character and return the appropriate value
  * Return: return 1 if c is a letter or 0 otherwise.
  */
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
