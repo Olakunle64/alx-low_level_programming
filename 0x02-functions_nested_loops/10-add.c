8b8bff7242c3fe964219928867c4a9f6cdddcd8c
Olakunle64 <olakunleisiaq50@gmail.com>
2023-06-21 07:38:02 +0000

    add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add two integers
+ * @a: first argument
+ * @b: second argument
+ *
+ * Description: This function is meant to add two
+ * integers
+ * Return: return 1 on success
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ * Description - This function is meant to print the
+ * 9 times table starting with 0
+ * Return: void
+ */
+void times_table(void)
+{
+
