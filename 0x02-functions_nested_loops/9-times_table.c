6779761708478d3696a1060da5b0a6816f20eb28
Olakunle64 <olakunleisiaq50@gmail.com>
2023-06-22 07:34:27 +0000

    I am very glad to see the result of my hardworking. I went far to acquire the knowlege of writing function to print 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,4 +7,32 @@
  */
 void times_table(void)
 {
+	int i, j;
+	int prod, check;
 
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			prod = i * j;
+			if (prod <= 9)
+			{
+				_putchar(prod + '0');
+			}
+			else
+			{
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				check = i * (j + 1);
+				if (!(check > 9))
+					_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
