80fd010ac9343e64da59adf8a8c84c339d540a03
Olakunle64 <olakunleisiaq50@gmail.com>
2023-06-26 21:48:41 +0000

    print a string in a reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,32 @@
 #include "main.h"
 
 /**
+ * _strlen - count the characters in a string
+ * @str: string
+ *
+ * Description: count the numbers of characters in a string
+ * Return: return the length of a string.
+ */
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
  * print_rev - print a string in reverse
  * @s: the string
  *
- * Description: print a string in reverse followed by a new line
+ * Description: print a string in reversee followed by a new line
  * Return: void
  */
+
 void print_rev(char *s)
 {
 	int len;
