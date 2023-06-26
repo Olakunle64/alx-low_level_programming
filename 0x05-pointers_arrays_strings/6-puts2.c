64508874e9b61cdead9ddb2fa040154a6b84a0f0
Olakunle64 <olakunleisiaq50@gmail.com>
2023-06-26 23:33:09 +0000

    add an if statement at the begining in task 6 to control the argument

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,14 +13,12 @@ void puts2(char *str)
 {
 	int i = 0;
 
+	if (*str == "")
+		break;
 	while (*(str + i) != '\0')
 	{
 		_putchar(*(str + i));
 		i = i + 2;
-		if (*(str + i) == '\0')
-		{
-			_putchar('\n');
-		}
 	}
 	_putchar('\n');
 }
