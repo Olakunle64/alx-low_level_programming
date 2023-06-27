8ddcc1c373b7d8d0e7432b771d841e269421e98d
Olakunle64 <olakunleisiaq50@gmail.com>
2023-06-27 00:23:00 +0000

    print half of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,10 +13,6 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	if (str[i] == '\0')
-	{
-
-	}
 	while (*(str + i) != '\0')
 	{
 		_putchar(*(str + i));
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * _strlen - count the characters in a string
+ * @s: string
+ *
+ * Descriptioin: count the number of characters in a string
+ * Return: return the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * puts_half - print half of a string
+ * @str: string
+ *
+ * Description: prints half of a string followed by a new line
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int len;
+	int n;
+	int len1;
+
+	len = _strlen(str);
+	n = (len - 1) / 2;
+	n++;
+	len1 = (len / 2);
+	if (len % 2 != 0)
+	{
+		while (str[n] != '\0')
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	else
+	{
+		while (str[len1] != '\0')
+		{
+			_putchar(str[len1]);
+			len1++;
+		}
+	}
+	putchar ('\n');
+}
