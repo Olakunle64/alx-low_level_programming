ebb675fc05b1017239272ff89c6765e586a313e6
Olakunle64 <olakunleisiaq50@gmail.com>
2023-06-28 19:02:53 +0000

    change s to str in task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,13 +14,13 @@ void puts2(char *str)
 	int i = 0;
 	int j = 0;
 
-	while (s[j] != '\0')
+	while (str[j] != '\0')
 	{
 		j++;
 	}
 	while (i < j)
 	{
-		_putchar(s[i]);
+		_putchar(str[i]);
 		i = i + 2;
 	}
 	_putchar('\n');
