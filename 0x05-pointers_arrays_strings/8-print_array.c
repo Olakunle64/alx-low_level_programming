2c8aabd1979295076546724a58f1e3f1eaae4d84
Olakunle64 <olakunleisiaq50@gmail.com>
2023-06-28 22:06:53 +0000

    control the argument n in task 8 prototype

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,13 +14,17 @@ void print_array(int *a, int n)
 {
 	int i;
 
+	if (n <= 0)
+	{
+		return;
+	}
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", *(a + i));
 		if (i != (n - 1))
 		{
-			_putchar(',');
-			_putchar(' ');
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	_putchar('\n');
