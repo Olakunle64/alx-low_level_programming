d15b9d248a724f80b3431872fac0d502b9526abf
Olakunle64 <olakunleisiaq50@gmail.com>
2023-06-29 16:16:06 +0000

    make some corrections in task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - count the character in a string
- * @s: argument
- *
- * Description: This function is meant to count the number of
- * characters of a string.
- * Return: return the length of a string.
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-	return (len);
-}
-
-/**
  * _strncpy - copy n bytes of src to dest
  * @dest: destination string
  * @src: source string
@@ -29,34 +9,22 @@ int _strlen(char *s)
  * Description: This function is meant to copy n bytes of src to dest
  * Return: return a pointer to the resulting dest string.
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-	int len2 = 0;
-	int i, sub, r_len;
+	int len2, i;
 
 	if (n <= 0)
 	{
 		return (dest);
 	}
-	r_len = _strlen(src);
-	while (src[len2] != src[n])
-	{
-		len2++;
-	}
-	for (i = 0; i < len2; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	if (n > r_len)
+	for (len2 = i; len2 < n; len2++)
 	{
-		sub = n - r_len;
-		while (sub > 1)
-		{
-			dest[i] = '\0';
-			i++;
-			sub--;
-		}
-		dest[i] = '\0';
+		dest[len2] = '\0';
 	}
 	return (dest);
 }
