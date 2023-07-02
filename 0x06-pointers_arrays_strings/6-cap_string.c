fb7f3a63717dfa4accbb7792db86cd5039676ca4
Olakunle64 <olakunleisiaq50@gmail.com>
2023-07-02 00:23:24 +0000

    capitalize all words in a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -32,9 +32,8 @@ char _strlen(char *str)
 char *cap_string(char *s)
 {
 	int i = 0;
-	int len = _strlen(s);
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 	if ((s[i] >= 97 && s[i] <= 122))
 	{
