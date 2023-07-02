0a62b4a91c26b8f42aa94e259eb0cc4710e8b82f
Olakunle64 <olakunleisiaq50@gmail.com>
2023-07-02 00:54:05 +0000

    encode a string into rot13 encryption

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,4 +1,33 @@
 #include "main.h"
 
 /**
+ * rot13 - Encode a string using rot13
+ * @s: string
  *
+ * Description: This function is meant to encode a string
+ * using rot13 encryption.
+ * Return: return a pointer to the string
+ */
+
+char *rot13(char *s)
+{
+	char fd;
+	char rep;
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (fd = 97, rep = 110; fd <= 110 && rep <= 122; fd++, rep++)
+		{
+			if (s[i] == fd)
+			{
+				s[i] = rep;
+			}
+			else if (s[i] == rep)
+			{
+				s[i] = fd;
+			}
+		}
+	}
+	return (s);
+}
