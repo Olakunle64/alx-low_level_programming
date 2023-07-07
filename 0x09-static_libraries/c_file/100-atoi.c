72db7340f7d882c347e14565d2d9a6d99f089fac
Olakunle64 <olakunleisiaq50@gmail.com>
2023-07-07 22:35:52 +0000

    d

---
+++ 0x09-static_libraries/c_file/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: string
+ *
+ * Description: This function is meant to convert a string to
+ * an integer
+ * Return: return an integer
+ */
+
+int _atoi(char *s)
+{
+	return (0);
+}
---
+++ 0x09-static_libraries/c_file/5-strstr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: The string
+ * @needle: substring
+ *
+ * Description: find the first occurrence of the substring needle
+ * in the string haystack. The terminating null bytes are not
+ * compared.
+ * Return: return a pointer to the beginning of the located
+ * substring or NULL if the substring is not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k;
+	int len1, len2;
+
+	len1 = 0;
+	len2 = 0;
+	while (haystack[len1] != '\0')
+	{
+		len1++;
+	}
+	while (needle[len2] != '\0')
+	{
+		len2++;
+	}
+	for (i = 0; i < len1; i++)
+	{
+		for (j = i, k = 0; k < len2; k++, j++)
+		{
+			if (haystack[j] != needle[k])
+			{
+				break;
+			}
+		}
+		if (k == len2)
+		{
+			return (haystack + i);
+		}
+	}
+	return (0);
+}
