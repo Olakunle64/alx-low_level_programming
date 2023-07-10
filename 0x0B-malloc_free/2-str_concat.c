623adcf4c67f61369fa45cfbce5df7f772af243f
Olakunle64 <olakunleisiaq50@gmail.com>
2023-07-10 23:07:10 +0000

    treat NULL as an empty string if an if statement

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -37,6 +37,10 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, len_1, len_2, T_len;
 	char *ptr;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	if (!(s1 && s2))
 		return (NULL);
 	len_1 = _strlen(s1);
@@ -46,19 +50,13 @@ char *str_concat(char *s1, char *s2)
 	ptr = malloc(T_len * sizeof(char));
 	if (ptr)
 	{
-		if (s1)
-		{
 		for (i = 0; i < len_1 - 1; i++)
 		{
 			ptr[i] = s1[i];
 		}
-		}
-		if (s2)
-		{
 		for (j = 0; j < len_2; j++)
 		{
 			ptr[i + j] = s2[j];
-		}
 		}
 		ptr[i + j] = '\0';
 		return (ptr);
