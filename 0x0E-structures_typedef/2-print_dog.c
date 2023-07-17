2f72c1f32b8ad3cb38a2be46127216f3af8faf57
Olakunle64 <olakunleisiaq50@gmail.com>
2023-07-17 22:03:40 +0000

    put a newline at the end of the if block

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,15 +17,15 @@ void print_dog(struct dog *d)
 
 	}
 	if (d->name == NULL)
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 	if (!(d->age))
-		printf("(nil)");
+		printf("(nil)\n");
 	else
 		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
-		printf("(nil)");
+		printf("(nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
