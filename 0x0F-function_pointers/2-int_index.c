8e148d1ac7a7430128107ddfb4ee548d956bacb8
Olakunle64 <olakunleisiaq50@gmail.com>
2023-07-19 15:50:48 +0000

    rearrange the if statement

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,12 +16,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (size <= 0)
+		return (-1);
 	if (cmp == NULL || array == NULL)
 	{
 		exit(EXIT_FAILURE);
 	}
-	if (size <= 0)
-		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
