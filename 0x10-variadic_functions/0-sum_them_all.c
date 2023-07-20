4159424b70e6293ae70cf6f4c233290124a3aed4
Olakunle64 <olakunleisiaq50@gmail.com>
2023-07-20 16:02:36 +0000

    change the data type of variable i from signed to unsigned

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,7 +14,7 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list num;
-	int i;
+	unsigned int i;
 	int sum = 0;
 
 	if (n == 0)
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list num;
-	int i;
+	unsigned int i;
 
 	va_start(num, n);
 	if (n == 0)
