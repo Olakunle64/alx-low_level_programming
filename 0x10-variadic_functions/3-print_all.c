3b1c6fe1ce326327a65bd89b34e06f72a73347f9
Olakunle64 <olakunleisiaq50@gmail.com>
2023-07-20 17:25:56 +0000

    print

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,6 +4,25 @@
 #include "variadic_functions.h"
 
 /**
+ * space_comma - print a comma followed by a space
+ * @j: first integer
+ * @count: second integer
+ *
+ * Description: this function is meant to print a comma
+ * followed by a space
+ * Return: void
+ */
+
+void space_comma(int j, int count)
+{
+	if (j != count - 1)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+}
+
+/**
  * print_all - print anything
  * @format: a list of types of arguments passed to the function
  *
@@ -14,4 +33,46 @@
 
 void print_all(const char * const format, ...)
 {
+	va_list call;
+	char c;
+	int i;
+	float f;
+	char *s;
+	int j = 0;
+	int count;
 
+	count = strlen(format);
+	va_start(call, format);
+	while (format[j])
+	{
+		switch(format[j])
+		{
+			case 'c':
+				c = (char)va_arg(call, int);
+				printf("%c", c);
+				space_comma(j, count);
+				break;
+			case 'i':
+				i = va_arg(call, int);
+				printf("%d", i);
+				space_comma(j, count);
+				break;
+			case 'f':
+				f = (float)va_arg(call, double);
+				printf("%f", f);
+				space_comma(j, count);
+				break;
+			case 's':
+				s = va_arg(call, char *);
+				if (s)
+					printf("%s", s);
+				else
+					printf("(nil)");
+				space_comma(j, count)
+				break;
+		}
+		j++;
+	}
+	putchar('\n');
+	va_end(call);
+}
