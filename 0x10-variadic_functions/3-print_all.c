95c5da8746d9e584c7fdb3713fa310f599cb6266
Olakunle64 <olakunleisiaq50@gmail.com>
2023-07-23 17:39:19 +0000

    add a newline incase format is null

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,7 +14,10 @@
 void __exit(const char * const format)
 {
 	if (format == NULL)
+	{
+		putchar('\n');
 		exit(EXIT_FAILURE);
+	}
 }
 
 /**
@@ -83,6 +86,9 @@ void print_all(const char * const format, ...)
 			case 's':
 				s = va_arg(call, char *);
 				print_a_string(s);
+			default:
+				k++;
+				break;
 		}
 		if (j != count - 1 && (format[j] == 'c' ||
 			format[j] == 's' || format[j] == 'i' || format[j] == 'f'))
