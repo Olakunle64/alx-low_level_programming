7b1ddc64e274be786aa570d7fb1447ae871de1a4
Olakunle64 <olakunleisiaq50@gmail.com>
2023-07-28 00:36:28 +0000

    print a statement before the main is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * pre_print - print a statement before the main is executed
+ *
+ * Return: void
+ */
+
+void pre_print()
+{
+	printf("You're beat! and yet, you must allow, \nI bore my
+			house upoin my back!\n");
+}
