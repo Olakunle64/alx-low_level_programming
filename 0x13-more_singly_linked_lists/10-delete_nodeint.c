bc66cb9e62af9ea19c31f863ae787d733d584da1
Olakunle64 <olakunleisiaq50@gmail.com>
2023-08-01 01:04:35 +0000

    correct the seg fault

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,7 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int n_count;
 	listint_t *k_track, *temp;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 	k_track = *head;
 	if (index == 0)
