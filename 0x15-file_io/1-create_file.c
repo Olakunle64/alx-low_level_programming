e6af00cf59778180d4955a48dbcf7cfce4e3594e
Olakunle64 <olakunleisiaq50@gmail.com>
2023-08-07 21:29:25 +0000

    include stdio.h

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 #include <fcntl.h>
 #include <stdlib.h>
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,3 +1,7 @@
 #include <stdio.h>
 #include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
 
+/**
+ *
