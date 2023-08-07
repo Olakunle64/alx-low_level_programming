46579258fb3570ebf3953035459a20f3c1fb99a3
Olakunle64 <olakunleisiaq50@gmail.com>
2023-08-07 16:59:06 +0000

    read and print a string of chars from a file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - read a test of file and print it to the
+ * POSIX standard output
+ * @filename: name of the file
+ * @letters: number of letters it should read and print
+ *
+ * Return: return 0 if the file can not be opened or
+ * read OR return 0 if the file is NULL or if write
+ * fails or does not write the expected amount of bytes
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_des;
+	ssize_t count, w_byte;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	file_des = open(filename, O_RDONLY);
+	if (file_des == -1)
+	{
+		close(file_des);
+		free(buffer);
+		return (0);
+	}
+	count = read(file_des, buffer, letters);
+	w_byte = write(1, buffer, count);
+	if (w_byte != count && w_byte == -1)
+	{
+		close(file_des);
+		free(buffer);
+		return (0);
+	}
+	close(file_des);
+	return (w_byte);
+}
