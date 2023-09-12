210a8c7abc25d5c968355f0adbe0d5b171d587ca
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-12 18:31:40 +0000

    print INT_MIN too

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,16 +10,18 @@
 
 void print_number(int n)
 {
-	if (n < 0)
+	unsigned m = n;
+
+	if (m < 0)
 	{
 		_putchar('-');
-		n = -n;
+		m = -m;
 	}
-	if (n < 10)
+	if (m < 10)
 	{
-		_putchar(n + '0');
+		_putchar(m + '0');
 		return;
 	}
-	print_number(n / 10);
-	_putchar((n % 10) + '0');
+	print_number(m / 10);
+	_putchar((m % 10) + '0');
 }
