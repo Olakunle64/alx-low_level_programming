eeb1fb0dfc2a5c4687031d3fb862a86322610fc9
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-14 22:38:06 +0000

    handle the case where you want to delete node at index 0

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -46,6 +46,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
+		if (*head)
+			(*head)->prev = NULL;
 		free(current);
 		return (1);
 	}
