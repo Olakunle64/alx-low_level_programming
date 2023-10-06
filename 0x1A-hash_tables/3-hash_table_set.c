603a16e5efd41e32135d1a81ce7caaf9323864e7
Olakunle64 <olakunleisiaq50@gmail.com>
2023-10-06 15:09:30 +0000

    check if table is null

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,7 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *item, *current;
 	unsigned long int index;
 
-	if (key == NULL || *key == '\0')
+	if (ht == NULL || ht->size == 0 || key == NULL || *key == '\0')
 		return (0);
 	item = malloc(sizeof(hash_node_t));
 	if (item == NULL)
