96b9d24ab33268a62cf46b8524e0480d28ba220e
Olakunle64 <olakunleisiaq50@gmail.com>
2023-10-06 15:49:11 +0000

    retrive a value asociated with a key from a hash table

--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -11,8 +11,22 @@
 int main(void)
 {
     hash_table_t *ht;
+    unsigned long int i;
 
     ht = hash_table_create(1024);
-    hash_table_set(ht, "hetairas", "mentioner");
+    hash_table_set(ht, "hetairas", "observe");
+    hash_table_set(ht, "mentioner", "collide");
+    hash_table_set(ht, "salay", "kunle");
+    hash_table_set(ht, "adeshina", "sefiu");
+    for (i = 0; i < ht->size; i++)
+    {
+	    if (ht->array[i] != NULL)
+	    {
+	    	printf("key: %s------value: %s\n", ht->array[i]->key, ht->array[i]->value);
+		if (ht->array[i]->next != NULL)
+			printf("key: %s------value: %s\n", ht->array[i]->next->key, ht->array[i]->next->value);
+	    }
+	    	
+    }
     return (EXIT_SUCCESS);
 }
