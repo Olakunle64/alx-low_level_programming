12fc843a1979cbd58e33a653c640211a80c18d51
Olakunle64 <olakunleisiaq@gmail.com>
2024-01-17 21:24:50 +0000

    implement the jump search algorithm with a singly linked list

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,4 +13,21 @@ int binary_recur(int *array, int value, int start, int end);
 void print_search(int *array, size_t start, size_t end);
 int jump_search(int *array, size_t size, int value);
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
+listint_t *jump_list(listint_t *list, size_t size, int value);
 #endif /* SEARCH_ALGOS_H */
